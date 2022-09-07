3af69347ce4c2a179118f78ab3a2da8af7657e44
Danaa4 <Danaa4@users.noreply.github.com>
2022-09-07 19:21:40 +0300

    Done

---
+++ flutter_first_project/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("DANA");
+          },
+        ),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 166, 161, 161),
+          title: Text('MY First Flutter App'),
+        ),
+        body: Center(
+          child: Text('Dana Essa Almutairi',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Color.fromARGB(255, 101, 21, 15),
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
